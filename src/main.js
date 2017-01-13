ca1a9ef0074b1c4da2919605869ba6b86f729e4d
Ilya Batozskiy <i.batozskiy@gmail.com>
2017-01-13 17:36:45 +0200

    Add initials for Redux

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+
 var NotesApp = require('./components/NotesApp.jsx');
+import reducers from './reducers';
 
 ReactDOM.render(
-    <NotesApp />,
+    <Provider store={createStore(reducers)}>
+        <NotesApp />
+    </Provider>,
     document.getElementById('mount-point')
 );
