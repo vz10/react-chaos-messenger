6e3a8da9714c241841fb74de61a0c7a06f2acd34
Ilya Batozskiy <ilya.batozskiy@cyren.com>
2017-02-07 16:20:37 +0200

    Not the best attempt to implement visJS Network object

--- src/components/MessageLayout.jsx
+++ src/components/MessageLayout.jsx
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { extractDomain } from '../utils/utils'
 
+import vis from 'vis';
+
 import { firebaseConnect, helpers } from 'react-redux-firebase'
 const { isLoaded, isEmpty, dataToJS } = helpers
 
@@ -17,8 +19,31 @@ var MessageLayout = React.createClass({
     defaultProps: {
         notes: []
     },
-    componentDidMount: function() {
-        this.props.initTodo();
+    componentDidUpdate: function() {
+      var {messages} = this.props;
+      if (!isEmpty(messages)){
+          var nodes = new vis.DataSet(Object.keys(messages).map(function(key, id){
+              return {id: messages[key].id, label: messages[key].text};
+            })),
+            ids = Object.keys(messages).map(function(key, id){return messages[key].id}),
+            connections = [];
+            ids.sort();
+            // create an array with edges
+            for (var i=0; i < ids.length-1; i++){
+              connections.push({from: ids[i], to: ids[i+1]});
+            }
+            var edges = new vis.DataSet(connections),
+                data = {
+                   nodes: nodes,
+                   edges: edges
+                 },
+               options = {
+                 autoResize: false,
+                 height: '100%',
+                 width: '100%'
+               };
+            new vis.Network(this.refs.grid, data, options);
+      }
     },
     showName: function(message){
       return message.name ? <div><strong>{message.name}</strong><hr/></div> : '';
@@ -38,28 +63,34 @@ var MessageLayout = React.createClass({
                 return messages[o].id < selected.id ? messages[o].id : 0;
               }));
         }
-
         return (
-            <div className="notes-grid columns" ref="grid">
-                {!isLoaded(messages) ?
-                    'Loading' : isEmpty(messages) ? 'No messages yet':
-                    Object.keys(messages).map(function(key, id){
-                        return (
-                            <Message
-                                key={key}
-                                color={messages[key].color}
-                                max_id={max_id}
-                                min_id={min_id}
-                                before_selected={before_selected}
-                                id={messages[key].id}>
-                                {me.showName(messages[key])}
-                                {messages[key].text}
-                            </Message>
-                        );
-                    })
-                }
+            <div className="messages-layout" ref="grid">
+
             </div>
         );
+        // return (
+        //     <div className="notes-grid columns" ref="grid">
+        //       {new vis.Network("network", data, options)}
+        //
+        //         {!isLoaded(messages) ?
+        //             'Loading' : isEmpty(messages) ? 'No messages yet':
+        //             Object.keys(messages).map(function(key, id){
+        //                 return (
+        //                     <Message
+        //                         key={key}
+        //                         color={messages[key].color}
+        //                         max_id={max_id}
+        //                         min_id={min_id}
+        //                         before_selected={before_selected}
+        //                         id={messages[key].id}>
+        //                         {me.showName(messages[key])}
+        //                         {messages[key].text}
+        //                     </Message>
+        //                 );
+        //             })
+        //         }
+        //     </div>
+        // );
     }
 });
 
